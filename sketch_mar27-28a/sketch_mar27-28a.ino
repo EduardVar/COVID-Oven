8436d4a2050dd0cd13b72312dd8e5829291a444f
Eduard Varshavsky <47066749+EduardVar@users.noreply.github.com>
2020-03-28 22:26:55 -0400

    Adds updated sketch. Need to figure out timer

--- sketch_mar27-28a/sketch_mar27-28a.ino
+++ sketch_mar27-28a/sketch_mar27-28a.ino
@@ -1,38 +1,53 @@
 #include <LiquidCrystal_I2C.h>
-
-
 #include <Wire.h>
 
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+
+// HEAT
+#define MAXDO   3
+#define MAXCS   4
+#define MAXCLK  5
+
+double lastTemp = -274;
+
+// initialize the Thermocouple
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
+
+// LCD
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-#define outputA 6
-#define outputB 7
-#define outputSW 5
+#define outputA 7
+#define outputB 8
+#define outputSW 6
 
+// RELAY
 int relayPin = 9;
 
-bool confirmed = false;
 
+
+// ENCODER
 int target = 0;
 int current = 0;
 int aState;
 int aLastState;
 
-int time = 0;
+bool confirmed = false;
 
 void setup() {
   // put your setup code here, to run once:
+  //ENCODER
   pinMode(outputA, INPUT);
   pinMode(outputB, INPUT);
   pinMode(outputSW, INPUT_PULLUP);
 
-  // RELAY
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, HIGH);
-
   Serial.begin(9600);
 
   aLastState = digitalRead(outputA);
+
+  // RELAY
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH);
 
   // LCD
   lcd.begin(16, 2);
@@ -40,7 +55,7 @@ void setup() {
   //lcd.print("Hello, World!");
 
   lcd.setCursor(0, 0);
-  lcd.print("Curr Temp: ");
+  lcd.print("Current: ");
 
   lcd.setCursor(0, 1);
   lcd.print("Target: ");
@@ -48,48 +63,63 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // RELAY
-  digitalWrite(relayPin, LOW);
-  delay(3000);
-  digitalWrite(relayPin, HIGH);
-  delay(3000);
+  // TEMPERATURE
+  /*
+  double c = thermocouple.readCelsius();
+  if (isnan(c)) {
+   Serial.println("Something wrong with thermocouple!");
+  } else {
+   Serial.print("C = ");
+   Serial.println(c);
+  }
+  */
 
+  /*
+  // RELAY
+  digitalWrite(relayPin, HIGH);
+  delay(1000);
+  digitalWrite(relayPin, HIGH);
+  delay(1000);
+*/
   
+  // SWITCH
   if (!digitalRead(outputSW)) {
     confirmed = !confirmed;
 
     if (confirmed) {
-      lcd.setCursor(14, 1);
-      lcd.print("ON");
+      lcd.setCursor(15, 1);
+      lcd.print("!");
     } else {
-      lcd.setCursor(13, 1);
-      lcd.print("OFF");
+      lcd.setCursor(15, 1);
+      lcd.print("X");
     }
 
     delay(2000);
     lcd.setCursor(13, 1);
     lcd.print("   ");
   }
-
   
-  if (confirmed && time >= 10000) {
-    if (current != target) {
+  if (confirmed) {
+    /*if (current != target) {
       if (current < target)
         current += 1;
       else if (current > target)
         current -= 1;
+      */
+
+    double c = thermocouple.readCelsius();
+
+    if (c != lastTemp) {
+      lcd.setCursor(9, 0);
+      lcd.print("     ");
+      lcd.setCursor(9, 0);
+      lcd.print(c, 1);
+      lcd.print((char)223);
+      lcd.print('C');
     }
 
-
-    lcd.setCursor(11, 0);
-    lcd.print("    ");
-    lcd.setCursor(11, 0);
-    lcd.print(current);
-    lcd.print((char)223);
-
-    time = 0;
-  } else {
-    time++;
+    lastTemp = c;
+    delay(1000);
   }
 
   aState = digitalRead(outputA);
@@ -108,9 +138,8 @@ void loop() {
     lcd.setCursor(8, 1);
     lcd.print(target);
     lcd.print((char)223);
+    lcd.print('C');
   }
-
-  
 
   aLastState = aState;
 }
